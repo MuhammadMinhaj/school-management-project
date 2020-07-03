8cb74dbdf470b6a0dcc92f1613500b0d26ef83c6
muhammad-minhaj <programmer.mdminhaj@gmail.com>
2020-07-04 01:27:16 +0600

    change something

--- controllers/webController.js
+++ controllers/webController.js
@@ -3,11 +3,12 @@ let WebModel = require('../models/WebModel')
 exports.webGetController = async(req,res,next)=>{
     try{
         let menu  = await Menu.find()
-        let webModel = await WebModel.findOne()
+        let webModel = await WebModel.find()
+
         res.render('pages/index.ejs',{
             title:'Web Explorer',
             menu,
-            webModel
+            webModel:webModel[0]
         })
     }catch(e){
         next(e)
